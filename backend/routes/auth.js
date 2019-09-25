b4f5d1946381ffed054112e1c3f50f0c0f214ab9
Abdellah Chahboune <abdellah_chahboune@inwi.ma>
2019-09-25 17:17:03 +0100

    Joi work fine

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,54 @@
 const router = require('express').Router();
 const User= require('../models/user.model.js');
 
-router.post('/register', async (req,res)=>{
+//Validation
+const Joi= require('@hapi/joi');
 
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-        console.log(savedUser);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+const userSchema= Joi.object().keys({
+    username: Joi.string()
+    .min(6)
+    .required(),
+    email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+    password: Joi.string()
+    .min(6)
+    .required()
+});
+
+router.post('/register', async (req,res)=> {
+
+    //User validation befor Save
+
+        const {value , error} = userSchema.validate(req.body);
+        if(error){
+            return res.status(400).send(error.details[0].message);
+        }else res.send(value);
+        
+   
+     
+    // userSchema.validate(req.body, (err, result)=> {
+    //     if(err){
+    //         console.log(err)
+    //         res.send('an error !!!')
+    //     }
+    //     console.log(result);
+    //     res.send(result);
+    // });
+
+    // const user = new User({
+    //     username: req.body.username,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    // try {
+    //     const savedUser = await user.save();
+    //     res.send(savedUser); 
+    //     console.log(savedUser);
+    // } catch (error) {
+    //     res.status(400).send(error);
+    // }
 });
 
 router.post('/login', (req,res)=>{
